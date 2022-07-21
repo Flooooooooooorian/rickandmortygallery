064c7b282b754aa7919ec81117dfdf3654a234fd
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-07-21 16:35:06 +0200

    Add useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import CharacterGallery from "./components/CharacterGallery";
 import Header from "./components/Header";
@@ -9,16 +9,26 @@ import axios from "axios";
 function App() {
 
     const [characters, setCharacter] = useState<Character[]>([])
-
-
-    axios.get("https://rickandmortyapi.com/api/characterff")
-        .then((response) => {return response.data})
-        .then((data) => {setCharacter(data.results)})
-        .catch((error) => {console.error(error)})
-
-
     const [searchText, setSearchText] = useState<string>("")
     const filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(searchText.toLowerCase()))
+
+    useEffect(() => {
+        getCharacters()
+    }, [])
+
+    const getCharacters = () => {
+        axios.get("https://rickandmortyapi.com/api/character")
+            .then((response) => {
+                return response.data
+            })
+            .then((data) => {
+                setCharacter(data.results)
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
     return (
         <div className="App">
             <Header/>
